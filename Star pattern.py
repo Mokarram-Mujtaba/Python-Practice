796c7ddf53bf0ae705b311b3b0959226f5ecf1f8
Mokarram Mujtaba <83273572+m-mujtaba07@users.noreply.github.com>
2021-05-11 13:56:10 +0530

    Add files via upload
    initial committee

---
+++ Star pattern.py
@@ -0,0 +1,60 @@
+x=int(input("ENter the number  of rows you want to print"))
+
+for i in range(1,x+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
+
+print("----------------------")
+
+for i in range(x,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
+
+
+for i in range(1,x+1):
+    for k in range(1,((x+1)-i)):
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
+print("----------------")
+
+for i in range(x,0,-1):
+    for k in range(1,((x+1)-i)):
+        print(" ",end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
+print("----------------")
+
+for i in range(x,0,-1):
+    for k in range(1,(x+1)-i):
+        print(" ",end=" ")
+    for j in range(1,(2*i-1)+1):
+        print("*",end=" ")
+    print("")
+print("----------------")
+
+for i in range(1,x+1):
+    for k in range(1,(x+1)-i):
+        print(" ",end=" ")
+    for j in range(1,(2*i-1)+1):
+        print("*",end=" ")
+    print("")
+print("----------------")
+
+for i in range(1,x+1):
+    for k in range(1,(x+1)-i):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
+print("----------------")
+for i in range(x,0,-1):
+    for k in range(1,(x+1)-i):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print("")
